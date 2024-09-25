2dffe49ba86b5d7732a02d0a9a8404db95a85925
tsotne <tsotnemeskhuri@gmail.com>
2024-09-25 21:32:47 +0400

    Add basic functionality of currency converter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("EUR");
+  const [to, setTo] = useState("USD");
+  const [output, setOutput] = useState("");
+
+  useEffect(() => {
+    async function Convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+      );
+
+      const data = await res.json();
+
+      setOutput(data.rates[to]);
+    }
+
+    Convert();
+  }, [amount, from, to]);
+
   return (
-    <div className="App">
-      <span>Currency Converter</span>
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={to} onChange={(e) => setTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{output}</p>
     </div>
   );
 }
