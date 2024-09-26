e67de72be7ff735c8832bd821247e0c5c7bc61af
tsotne <tsotnemeskhuri@gmail.com>
2024-09-26 18:46:31 +0400

    Add loading state, functionality to autofocus input and use debounced amount value for data fetching

--- src/App.js
+++ src/App.js
@@ -1,45 +1,75 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function App() {
-  const [amount, setAmount] = useState(1);
+  const [amount, setAmount] = useState("");
+  const [debouncedAmount, setDebouncedAmount] = useState(amount);
   const [from, setFrom] = useState("EUR");
   const [to, setTo] = useState("USD");
   const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    async function Convert() {
+    const timer = setTimeout(() => {
+      setDebouncedAmount(Number(amount));
+    }, 300);
+
+    return () => clearInterval(timer);
+  }, [amount]);
+
+  useEffect(() => {
+    async function convert() {
+      setIsLoading(true);
       const res = await fetch(
-        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+        `https://api.frankfurter.app/latest?amount=${debouncedAmount}&from=${from}&to=${to}`
       );
 
       const data = await res.json();
 
       setOutput(data.rates[to]);
+      setIsLoading(false);
     }
 
-    Convert();
-  }, [amount, from, to]);
+    if (from === to) {
+      return setOutput(debouncedAmount);
+    }
+
+    convert();
+  }, [debouncedAmount, from, to]);
+
+  const amountInputRef = useRef(null);
+  useEffect(() => {
+    amountInputRef.current.focus();
+  }, []);
 
   return (
     <div>
       <input
         type="text"
+        ref={amountInputRef}
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
       />
-      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+      <select
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={to} onChange={(e) => setTo(e.target.value)}>
+      <select
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{output}</p>
+      <p>{isLoading || !debouncedAmount ? "..." : `${output} ${to}`}</p>
     </div>
   );
 }
